3117193058eb9c28cb8f37d439693f5729328f8a
Toru Hisai <toru@kokohead.(none)>
2010-09-15 10:55:24 -1000

    added db paramter

--- xml-store.scm
+++ xml-store.scm
@@ -12,36 +12,38 @@
 
 ;; seed (id-stack . proc)
 
-(define (set-parent! id parent-id)
+#;(define (set-parent! db id parent-id)
   (print `(set-parent! ,id ,parent-id)))
 
-(define (set-next-sibling! id sibling-id)
+(define (set-next-sibling! db id sibling-id)
   (print `(set-next-sibling! ,id ,sibling-id)))
 
-(define (eat-new-level-seed new-id)
+(define (add-element! db id elem-gi parent-id attributes)
+  (print `(add-element! ,id ,elem-gi <- ,parent-id ,attributes)))
+
+(define (eat-new-level-seed db new-id)
   (lambda (elem-gi attributes namespaces expected-content seed)
     (let ((id (new-id))
           (parent-id (car (car seed)))
           (proc (cdr seed)))
-      (print `(,id ,elem-gi parent: ,parent-id ,attributes ,expected-content))
+      (add-element! db id elem-gi parent-id attributes)
       (proc id)
-      (cons (cons id (car seed)) (lambda (child) (set-parent! child id))))))
+      (cons (cons id (car seed)) (lambda (child) #;(set-parent! db child id))))))
 
-(define (eat-finish-element elem-gi attributes namespaces parent-seed seed)
-  (let ((id (car (car seed)))
-        (parent-id (cadr (car seed))))
-    (print `(/ ,id ,elem-gi parent: ,parent-id))
-    (cons (car parent-seed)
-          (lambda (sibling)
-            (set-parent! sibling parent-id)
-            (set-next-sibling! id sibling)))))
+(define (eat-finish-element db)
+  (lambda (elem-gi attributes namespaces parent-seed seed)
+    (let ((id (car (car seed)))
+          (parent-id (cadr (car seed))))
+      ;; (print `(/ ,id ,elem-gi parent: ,parent-id))
+      (cons (car parent-seed)
+            (lambda (sibling)
+              #;(set-parent! db sibling parent-id)
+              (set-next-sibling! db id sibling))))))
 
-(define (p new-id)
+(define (p db new-id)
   (ssax:make-parser
-   NEW-LEVEL-SEED (eat-new-level-seed new-id)
-
-   FINISH-ELEMENT eat-finish-element
-
+   NEW-LEVEL-SEED (eat-new-level-seed db new-id)
+   FINISH-ELEMENT (eat-finish-element db)
    CHAR-DATA-HANDLER (lambda (string1 string2 seed)
                        seed)
    ))
@@ -49,4 +51,7 @@
 (define doc
   (sxml:sxml->xml '(a (b (c)) (d (@ (a 123))) (e))))
 
-((p (make-new-id)) (open-input-string (tree->string doc)) (cons (list 'TOP) (lambda (id) (print `(TOP ,id)))))
+((p () (make-new-id))
+ (open-input-string (tree->string doc))
+ (cons (list 'TOP)
+       (lambda (id) (print `(TOP ,id)))))
