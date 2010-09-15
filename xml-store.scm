537019eba95205786a599212a192428f2763d11a
Toru Hisai <toru@kokohead.(none)>
2010-09-14 19:14:01 -1000

    added set-parent! and set-next-sibling!

--- xml-store.scm
+++ xml-store.scm
@@ -1,26 +1,52 @@
 (use text.tree)
 (use sxml.tools)
 (use sxml.ssax)
+(use dbm)
+(use dbm.fsdbm)
 
-(define new-id
+(define (make-new-id)
   (let ((i 0))
     (lambda ()
       (inc! i)
       i)))
 
-(define p
+;; seed (id-stack . proc)
+
+(define (set-parent! id parent-id)
+  (print `(set-parent! ,id ,parent-id)))
+
+(define (set-next-sibling! id sibling-id)
+  (print `(set-next-sibling! ,id ,sibling-id)))
+
+(define (eat-new-level-seed new-id)
+  (lambda (elem-gi attributes namespaces expected-content seed)
+    (let ((id (new-id))
+          (parent-id (car (car seed)))
+          (proc (cdr seed)))
+      (print `(,id ,elem-gi parent: ,parent-id ,attributes ,expected-content))
+      (proc id)
+      (cons (cons id (car seed)) (lambda (child) (set-parent! child id))))))
+
+(define (eat-finish-element elem-gi attributes namespaces parent-seed seed)
+  (let ((id (car (car seed)))
+        (parent-id (cadr (car seed))))
+    (print `(/ ,id ,elem-gi parent: ,parent-id))
+    (cons (car parent-seed)
+          (lambda (sibling)
+            (set-parent! sibling parent-id)
+            (set-next-sibling! id sibling)))))
+
+(define (p new-id)
   (ssax:make-parser
-   NEW-LEVEL-SEED (lambda (elem-gi attributes namespaces expected-content seed)
-                    (let ((id (new-id)))
-                      (print `(,id ,elem-gi ,seed))
-                      (cons id seed)))
-   FINISH-ELEMENT (lambda (elem-gi attributes namespaces parent-seed seed)
-                    (print `(/ ,(car seed) ,elem-gi ,parent-seed ,seed))
-                    parent-seed)
-   CHAR-DATA-HANDLER (lambda (string1 string2 seed))
+   NEW-LEVEL-SEED (eat-new-level-seed new-id)
+
+   FINISH-ELEMENT eat-finish-element
+
+   CHAR-DATA-HANDLER (lambda (string1 string2 seed)
+                       seed)
    ))
 
 (define doc
-  (sxml:sxml->xml '(a (b (c)) (d) (e))))
+  (sxml:sxml->xml '(a (b (c)) (d (@ (a 123))) (e))))
 
-(p (open-input-string (tree->string doc)) ())
+((p (make-new-id)) (open-input-string (tree->string doc)) (cons (list 'TOP) (lambda (id) (print `(TOP ,id)))))
